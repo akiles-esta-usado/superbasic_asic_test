4973159c1af937d74e41ab81547b661fc26afb67
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2022-05-12 10:53:49 -0400

    Ocupados scripts de iic-oss-tools, y referencian al contenedor del equipo

---
+++ start_vnc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Harald Pretl & Georg Zachl, IIC, JKU, 2022
+
+# SET YOU DESIGN PATH RIGHT!
+if [ -z ${DESIGNS+z} ]; then
+        DESIGNS=$PWD
+
+	if [ ! -d "$DESIGNS" ]; then
+		mkdir -p "$DESIGNS"
+	fi
+
+	echo "Design directory auto-set to $DESIGNS"
+fi
+
+# Set the host ports, disable with 0. Only used if not set as a shell variables!
+if [ -z ${WEBSERVER_PORT+z} ]; then
+	WEBSERVER_PORT=80
+fi
+if [ -z ${VNC_PORT+z} ]; then
+	VNC_PORT=5901
+fi
+
+if [ -z ${DOCKER_USER+z} ]; then
+	DOCKER_USER="akilesalreadytaken"
+fi
+
+if [ -z ${DOCKER_IMAGE+z} ]; then
+	DOCKER_IMAGE="iic-osic-tools-analog"
+fi
+
+if [ -z ${DOCKER_TAG+z} ]; then
+	DOCKER_TAG="latest"
+fi
+
+PORT_PARAMS=""
+if [ $WEBSERVER_PORT -gt 0 ]; then
+	PORT_PARAMS="$PORT_PARAMS -p $WEBSERVER_PORT:80"
+fi
+if [ $VNC_PORT -gt 0 ]; then
+	PORT_PARAMS="$PORT_PARAMS -p $VNC_PORT:5901"
+fi
+
+docker run \
+	-d \
+	--rm \
+	--user $(id -u):$(id -g) \
+	$PORT_PARAMS \
+	-v $DESIGNS:/foss/designs:rw \
+	${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG}
---
+++ start_x.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Harald Pretl & Georg Zachl, IIC, JKU, 2022
+
+# SET YOU DESIGN PATH RIGHT!
+if [ -z ${DESIGNS+z} ]; then
+        DESIGNS=$PWD
+
+	if [ ! -d "$DESIGNS" ]; then
+		mkdir -p "$DESIGNS"
+	fi
+
+	echo "Design directory auto-set to $DESIGNS"
+fi
+
+if [ -z ${DOCKER_USER+z} ]; then
+	DOCKER_USER="akilesalreadytaken"
+fi
+
+if [ -z ${DOCKER_IMAGE+z} ]; then
+	DOCKER_IMAGE="iic-osic-tools-analog"
+fi
+
+if [ -z ${DOCKER_TAG+z} ]; then
+	DOCKER_TAG="latest"
+fi
+
+PARAMS=""
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	echo "Auto detected Linux"
+	# Should also be a senseful default
+	if [ -z ${XSOCK+z} ]; then
+		if [ -d "/tmp/.X11-unix" ]; then
+			XSOCK="/tmp/.X11-unix"
+		else
+			echo "X socket could not be found. Please set manually!"
+			exit 1
+		fi
+	fi
+	PARAMS="$PARAMS -v $XSOCK:/tmp/.X11-unix:rw"
+	if [ -z ${DISP+z} ]; then
+		if [ -z ${DISPLAY+z} ]; then
+			echo "No DISPLAY set"
+			exit 1
+		else
+			DISP=$DISPLAY
+		fi
+	fi
+	if [ -z ${XAUTH+z} ]; then
+		# Senseful defaults (uses XAUTHORITY Shell variable if set, or the default .Xauthority -file in the caller home directory)
+		if [ -z ${XAUTHORITY+z} ]; then
+			if [ -f "$HOME/.Xauthority" ]; then
+				XAUTH="$HOME/.Xauthority"
+			else
+				echo "Xauthority could not be found. Please set manually!"
+				exit 1
+			fi
+		else
+			XAUTH=$XAUTHORITY
+		fi
+		# Thanks to https://stackoverflow.com/a/25280523
+		XAUTH_TMP="/tmp/.iic-osic-docker_xauthority"
+		#create empty file
+		echo -n > ${XAUTH_TMP}
+		xauth -f ${XAUTH} nlist ${DISP} | sed -e 's/^..../ffff/' | xauth -f ${XAUTH_TMP} nmerge -
+		XAUTH=${XAUTH_TMP}
+	fi
+	PARAMS="$PARAMS -v $XAUTH:/headless/.xauthority:rw -e XAUTHORITY=/headless/.xauthority"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	if [ -z ${DISP+z} ]; then
+		DISP="host.docker.internal:0"
+		if [[ $(type -P "xhost") ]]; then
+			xhost +localhost
+		else
+			echo "WARNING: xhost could not be found, access control to the X server must be managed manually!"
+		fi
+	fi
+else
+	echo "Not setup for $OSTYPE"
+	exit 1
+fi
+
+
+# Finally, run the container, sets DISPLAY to the local display number
+docker run \
+	-d \
+	--rm \
+	--user $(id -u):$(id -g) \
+	-e DISPLAY=$DISP \
+	-v $DESIGNS:/foss/designs:rw \
+	$PARAMS \
+	${DOCKER_USER}/${DOCKER_IMAGE}:${DOCKER_TAG}
+
